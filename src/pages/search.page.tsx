3bffd94b9d2a70dae5d484fb7875e1ed11bb6014
PSAnton <ap@productshare.shop>
2024-12-17 21:30:47 -0500

    Test commit

--- src/pages/search.page.tsx
+++ src/pages/search.page.tsx
@@ -2,7 +2,7 @@ import Page, { getServerSideProps } from '@/src/pages/[channel]/search.page';
 import { Redirect } from '@/src/lib/redirect';
 import React from 'react';
 import type { InferGetServerSidePropsType } from 'next';
-
+//test comment
 export default (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return Redirect({ children: <Page {...props} /> })();
 };
